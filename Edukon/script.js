2c07b6f16906f9560148a32b3dd99d053492bbeb
Syed Usman Hasany <156933453+syedusmanhasany@users.noreply.github.com>
2025-02-24 00:17:08 +0500

    Update script.js

--- Edukon/script.js
+++ Edukon/script.js
@@ -2,6 +2,5 @@ let nav_icon = document.getElementById("nav");
 let nav_links = document.querySelector(".links");
 
 nav_icon.addEventListener("click", () => {
-    // Toggle the "active" class on the .links element
     nav_links.classList.toggle("active");
 });
